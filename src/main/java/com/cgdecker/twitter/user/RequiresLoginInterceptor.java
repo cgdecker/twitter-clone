b03ac6c223c3c23031793dfd106099faa14ffaf8
Colin Decker <cgdecker@gmail.com>
2011-03-24 21:42:46 -0400

    RequiresLoginInterceptor can now return proper redirect for service methods returning Reply<?>

--- src/main/java/com/cgdecker/twitter/user/RequiresLoginInterceptor.java
+++ src/main/java/com/cgdecker/twitter/user/RequiresLoginInterceptor.java
@@ -1,5 +1,6 @@
 package com.cgdecker.twitter.user;
 
+import com.google.sitebricks.headless.Reply;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
@@ -9,6 +10,7 @@ import javax.inject.Provider;
 /**
  * @author cgdecker@gmail.com (Colin Decker)
  */
+// TODO: Move this somewhere under web
 public class RequiresLoginInterceptor implements MethodInterceptor {
   private Provider<UserSession> userSessionProvider;
 
@@ -17,8 +19,12 @@ public class RequiresLoginInterceptor implements MethodInterceptor {
   }
 
   @Override public Object invoke(MethodInvocation invocation) throws Throwable {
-    if (!userSessionProvider.get().isLoggedIn())
-      return "/login"; // TODO: Move this somewhere under web
+    if (!userSessionProvider.get().isLoggedIn()) {
+      if (Reply.class.isAssignableFrom(invocation.getMethod().getReturnType())) {
+        return Reply.saying().redirect("/login");
+      }
+      return "/login";
+    }
     return invocation.proceed();
   }
 }
